39f6bf184ebab5861b625777641ecf2a0ed9c321
Nellyjohn <okpala_vivian@.com>
2023-10-16 04:49:43 -0700

    Function that handles the %s format specifier

---
+++ _istring.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+char _strlen(char *s);
+char *_strcpy(char *str, char *s);
+
+/**
+  * _strcpy - cpoies string 2 into sring 1
+  * @str: string 1
+  * @s: string 2
+  * Return: pointer to string 1
+  */
+char *_strcpy(char *str, char *s)
+{
+	int index = 0;
+
+	while (s != '\0')
+	{
+		str[index] = s[index];
+		index++;
+	}
+
+	return (str);
+}
+
+/**
+  * _strlen - prints the length of a string
+  * @s: string
+  * Return: length of the string
+  */
+char _strlen(char *s)
+{
+	int len = 0;
+
+	while (s != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
+  * _istring - prints a string
+  * @list: list of arguments
+  * Return: copy of the string
+  */
+char _istring(va_list list)
+{
+	char *str;
+	char *s = va_arg(list, char *);
+
+
+	if (s == NULL)
+		s = "(NULL)";
+
+	str = malloc(sizeof(char) * (_strlen(s) + 1));
+	if (!str)
+		return (NULL);
+
+	return (_strcpy(str, s));
+}
