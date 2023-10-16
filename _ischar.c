5f4c10aee3919992478ac262958c4b84b67f5501
fidelcom <okpara2040@gmail.com>
2023-10-16 11:01:04 +0000

    function that handle char

---
+++ _ischar.c
@@ -0,0 +1,29 @@
+#include <main.h>
+
+/**
+ * _ischar - function return char as string
+ * @list: char value
+ * Return: return string
+ */
+
+char _ischar(va_list list)
+{
+	char *str, alpha;
+
+	alpha = va_arg(list, int);
+
+	if (alpha == 0)
+	{
+		alpha = '\0';
+	}
+
+	str = malloc(sizeof(char) * 2);
+
+	if (!str)
+	{
+		return (NULL);
+	}
+	str[0] = alpha;
+	str[1] = '\0';
+	return (str);
+}
