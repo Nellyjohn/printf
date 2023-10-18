278a245d47e4449134c0b0993a448f03cce2d73e
fidelcom <okpara2040@gmail.com>
2023-10-18 10:39:45 +0000

    Debugging _isinteger function

--- _isinteger.c
+++ _isinteger.c
@@ -28,6 +28,25 @@ char *int_to_string(int len, int digit)
 	str = malloc(sizeof(char) * (len + 2));
 	if (!str)
 		return (NULL);
+	if (digit == INT_MIN)
+    {
+        str = malloc(sizeof(char) * 12);
+        if (!str)
+            return (NULL);
+        str[0] = '-';
+        str[1] = '2';
+        str[2] = '1';
+        str[3] = '4';
+        str[4] = '7';
+        str[5] = '4';
+        str[6] = '8';
+        str[7] = '3';
+        str[8] = '6';
+        str[9] = '4';
+        str[10] = '8';
+        str[11] = '\0';
+        return str;
+    }
 	if (digit < 0)
 	{
 		digit = -digit;
