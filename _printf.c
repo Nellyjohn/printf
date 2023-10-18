0da6e97d9ff3d206abbc3a20f3d303625ef2585d
fidelcom <okpara2040@gmail.com>
2023-10-18 23:40:17 +0000

    Debugging printf

--- _printf.c
+++ _printf.c
@@ -6,99 +6,96 @@
  * @len: sting position in buffer
  * Return: position
  */
-
-int buffer_overflow_check(char *ptr, int len)
+int buffer_overflow_check(char *buffer, int len)
 {
-	if (len > 1024)
+	if (len > 1020)
 	{
-		write(1, ptr, len);
+		write(1, buffer, len);
 		len = 0;
 	}
 	return (len);
 }
 
 /**
- * _printf - custome printf function
- * @format: string to be printed with it's idntifiers
- * Return: string
+ * _printf - mini printf version
+ * @format: initial string with all identifiers
+ * Return: strings with identifiers expanded
  */
-
 int _printf(const char *format, ...)
 {
-	int width = 0, size = 0, i = 0, j = 0;
+	int len = 0, size = 0, i = 0, j = 0;
 	va_list list;
-	char *bf, *str;
+	char *buffer, *str;
 	char* (*fn)(va_list);
 
-	if (!format)
+	if (format == NULL)
 		return (-1);
-	bf = make_buffer();
-	if (!bf)
+
+	buffer = make_buffer();
+	if (buffer == NULL)
 		return (-1);
+
 	va_start(list, format);
 
 	while (format[i] != '\0')
 	{
-		if (format[i] != '%')
+		if (format[i] != '%') /* copy format into buffer until '%' */
 		{
-			width = buffer_overflow_check(bf, width);
-			bf[width++] = format[i++];
+			len = buffer_overflow_check(buffer, len);
+			buffer[len++] = format[i++];
 			size++;
 		}
-		else
+		else /* if %, find function */
 		{
 			i++;
-			if (format[i] == '\0')
+			if (format[i] == '\0') /* handle single ending % */
 			{
 				va_end(list);
-				free(bf);
+				free(buffer);
 				return (-1);
 			}
-			if (format[i] == '%')
+			if (format[i] == '%') /* handle double %'s */
 			{
-				width = buffer_overflow_check(bf, width);
-				bf[width++] = format[i];
+				len = buffer_overflow_check(buffer, len);
+				buffer[len++] = format[i];
 				size++;
 			}
 			else
 			{
-				fn = select_function(format[i]);
-				if (!fn)
+				fn = select_function(format[i]); /* grab function */
+				if (fn == NULL)  /* handle fake id */
 				{
-					width = buffer_overflow_check(bf, width);
-					bf[width++] = '%';
-					size++;
-					bf[width++] = format[i];
-					size++;
+					len = buffer_overflow_check(buffer, len);
+					buffer[len++] = '%'; size++;
+					buffer[len++] = format[i]; size++;
 				}
-				else
+				else /* return string, copy to buffer */
 				{
 					str = fn(list);
-					if (!str)
+					if (str == NULL)
 					{
 						va_end(list);
-						free(bf);
+						free(buffer);
 						return (-1);
 					}
-					if (format[i] == 'c' && str[0] != '\0')
+					if (format[i] == 'c' && str[0] == '\0')
 					{
-						width = buffer_overflow_check(bf, width);
-						bf[width++] = '\0';
+						len = buffer_overflow_check(buffer, len);
+						buffer[len++] = '\0';
 						size++;
 					}
+					j = 0;
 					while (str[j] != '\0')
 					{
-						width = buffer_overflow_check(bf, width);
-						bf[width++] = str[j];
-						size++;
-						j++;
+						len = buffer_overflow_check(buffer, len);
+						buffer[len++] = str[j];
+						size++; j++;
 					}
 					free(str);
 				}
-			}
-			i++;
+			} i++;
 		}
 	}
-	print_buffer(bf, width, list);
+	print_buffer(buffer, len, list);
 	return (size);
 }
--- main.c
+++ main.c
@@ -11,9 +11,12 @@ int main(void)
 {
 	int len, len2;
 	void *p = (void *)0x7fff5100b6f8;
+	void *p2 = (void *)0x7faf51f0f608;
+	void *p3 = (void *)0x6ff42510b6f8;
+	void *p4 = (void *)0x7fff510236f8;
 
-	len = _printf("Can you print an address?\n%p\nNice!\n", p);
-	len2 = printf("Can you print an address?\n%p\nNice!\n", p);
+	len = _printf("Can you print several addresses?\n%p,%p,%p,%p\nNice!\n", p, p2, p3, p4);
+	len2 = printf("Can you print several addresses?\n%p,%p,%p,%p\nNice!\n", p, p2, p3, p4);
 	fflush(stdout);
 	if (len != len2)
 	{
