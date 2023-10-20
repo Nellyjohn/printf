ee41bea899059daa8fafc019620a09e0b8bdb322
fidelcom <okpara2040@gmail.com>
2023-10-20 15:49:03 +0000

    Debugging

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ int main(void)
 {
 	int len, len2;
 
-	len = _printf("%x", -1024);
-	len2 = printf("%x", -1024);
+	len = _printf("%x", 0);
+	len2 = printf("%x", 0);
 	fflush(stdout);
 	if (len != len2)
 	{
